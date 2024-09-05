a76e3042cbcb1846536d4ba7da5178dd1517a08f
NuttyShrimp <jan.lecoutere@hotmail.be>
2024-09-05 17:03:51 +0200

    feat(tests): add public permission test

--- loama/tests/permission-table.spec.ts
+++ loama/tests/permission-table.spec.ts
@@ -4,7 +4,7 @@ import authSetup from "./auth.setup";
 test.beforeEach(authSetup);
 
 test.describe("Permission table", () => {
-    test("Can add webId subject", async ({ page }) => {
+    test("Can add & remove webId subject", async ({ page }) => {
         await page.getByText("README").click();
 
         // Check if right panel is loaded
@@ -13,8 +13,8 @@ test.describe("Permission table", () => {
         await expect(listHeader).toContainText("Subjects with permissions")
 
         const startAgentPanelList = page.getByTestId('sidepanel-permission-list')
-        expect(startAgentPanelList.getByRole("listitem")).toHaveCount(2);
-        expect(startAgentPanelList.getByRole("listitem")).toHaveText(["http://localhost:8080/pod1/profile/card#me", "Public"])
+        await expect(startAgentPanelList.getByRole("listitem")).toHaveCount(2);
+        await expect(startAgentPanelList.getByRole("listitem")).toHaveText(["http://localhost:8080/pod1/profile/card#me", "Public"])
 
         await rightPanelElem.getByRole("button", { name: "Edit" }).click();
 
@@ -29,11 +29,71 @@ test.describe("Permission table", () => {
         await webIdInput.click();
         await webIdInput.fill("http://localhost:8080/pod2/profile/card#me")
         await newSubjectDrawer.getByRole("button", { name: "Create" }).click();
+        await expect(newSubjectDrawer).toBeHidden({ timeout: 30000 });
 
-        expect(newSubjectDrawer).toBeHidden({ timeout: 10000 });
+        const newSubjectRow = tableDrawer.locator("tr", { hasText: "http://localhost:8080/pod2/profile/card#me" });
+        await expect(newSubjectRow).toBeVisible();
+        await newSubjectRow.getByRole("button", { name: "Edit" }).click();
 
-        const newSubjectEntry = tableDrawer.getByText("http://localhost:8080/pod2/profile/card#me");
+        const subjectEditorDrawer = page.locator(".subject-drawer");
+        await expect(subjectEditorDrawer).toBeVisible()
 
-        expect(newSubjectEntry).toBeVisible();
+        // The new subject only should have the read permission
+        // We uncheck it to remove the subject so we are back to the previous state
+        await subjectEditorDrawer.getByLabel('Read').uncheck();
+        await expect(subjectEditorDrawer.getByLabel('Write')).not.toBeChecked();
+        await expect(subjectEditorDrawer.getByLabel('Append')).not.toBeChecked();
+        await expect(subjectEditorDrawer.getByLabel('Control')).not.toBeChecked();
+
+        // Wait until the operation is finished
+        await expect(subjectEditorDrawer.getByLabel('Read')).toBeEnabled();
+
+        await subjectEditorDrawer.locator("button.p-drawer-close-button").click();
+        await expect(subjectEditorDrawer).toBeHidden();
+
+        // Check if the table is updated & close the drawer
+        await expect(tableDrawer.locator("tbody").getByRole("row")).toHaveCount(2);
+        await tableDrawer.locator("button.p-drawer-close-button").click();
+        await expect(tableDrawer).toBeHidden();
+
+        // Check if the right side panel has the 2 subjects
+        await expect(startAgentPanelList.getByRole("listitem")).toHaveCount(2);
+        await expect(startAgentPanelList.getByRole("listitem")).toHaveText(["http://localhost:8080/pod1/profile/card#me", "Public"])
+    });
+
+    test("Can change public permissions", async ({ page, browser }) => {
+        await page.getByText("README").click();
+
+        // Check if right panel is loaded
+        const rightPanelElem = page.locator(".right-panel");
+        const listHeader = rightPanelElem.locator(".list-header");
+        await expect(listHeader).toContainText("Subjects with permissions")
+
+        await rightPanelElem.getByRole("button", { name: "Edit" }).click();
+
+        const tableDrawer = page.locator(".permission-drawer");
+        const publicSubjectRow = tableDrawer.locator("tr", { hasText: "Public" });
+        await expect(publicSubjectRow).toBeVisible();
+        await publicSubjectRow.getByRole("button", { name: "Edit" }).click();
+
+        const subjectEditorDrawer = page.locator(".subject-drawer");
+        await expect(subjectEditorDrawer).toBeVisible()
+        const readPermCheckbox = subjectEditorDrawer.getByLabel('Read')
+        // Everyone can read the file
+        await expect(readPermCheckbox).toBeChecked();
+
+        const podPage = await browser.newPage();
+        await podPage.goto("http://localhost:8080/pod1/README");
+        await expect(podPage.getByText("Welcome to your pod")).toBeVisible();
+
+        // Remove read permission
+        await readPermCheckbox.uncheck();
+        await expect(readPermCheckbox).toBeEnabled();
+
+        await podPage.reload();
+        await expect(podPage.getByText("Not logged in")).toBeVisible();
+
+        await readPermCheckbox.check();
+        await expect(readPermCheckbox).toBeEnabled();
     });
 })
