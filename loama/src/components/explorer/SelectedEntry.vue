76a955b0f79d56b15a9dd37c0896bbf5545279c2
NuttyShrimp <jan.lecoutere@hotmail.be>
2024-12-06 15:18:21 +0100

    feat(loama): add content viewer

--- loama/src/components/explorer/SelectedEntry.vue
+++ loama/src/components/explorer/SelectedEntry.vue
@@ -4,7 +4,10 @@
             <ExplorerEntity :is-container="podStore.selectedEntry.isContainer">
                 {{ podStore.selectedEntry.name }}
             </ExplorerEntity>
-            <PhXCircle :size="40" @click="$emit('close')" class="clickable" />
+            <div class="header-actions">
+                <PhEye :size="40" class="clickable" @click="() => contentDrawerVisible = true" />
+                <PhXCircle :size="40" @click="$emit('close')" class="clickable" />
+            </div>
         </header>
         <section>
             <div class="list-header">
@@ -22,6 +25,10 @@
             class="permission-drawer">
             <SubjectPermissionTable />
         </Drawer>
+        <Drawer v-model:visible="contentDrawerVisible" header="View content" position="right" class="content-drawer">
+            <div class="editor" ref="editorElement">
+            </div>
+        </Drawer>
     </div>
     <p v-else>No entry selected in the resource explorer, this shouldn't be possible!</p>
 </template>
@@ -29,18 +36,45 @@
 <script setup lang="ts">
 import { usePodStore } from '@/lib/state';
 import ExplorerEntity from './ExplorerEntity.vue';
-import { PhPencil, PhXCircle } from '@phosphor-icons/vue';
+import { PhEye, PhPencil, PhXCircle } from '@phosphor-icons/vue';
 import { activeController } from 'loama-controller';
 import LoButton from '../LoButton.vue';
-import { ref } from 'vue';
+import { ref, shallowRef, watch } from 'vue';
 import Drawer from 'primevue/drawer';
 import SubjectPermissionTable from './SubjectPermissionTable.vue';
+import '@/lib/monacoUserWorker'
+import * as monaco from 'monaco-editor';
+import { getDefaultSession } from '@inrupt/solid-client-authn-browser';
+import { getFile } from '@inrupt/solid-client';
 
 const podStore = usePodStore();
 
 defineEmits<{ close: [] }>()
 
 const permissionDrawerVisible = ref(false);
+const contentDrawerVisible = ref(false);
+const editorElement = ref<HTMLElement | null>(null);
+const editor = shallowRef<monaco.editor.IStandaloneCodeEditor | null>(null);
+
+watch(editorElement, async () => {
+    if (!editorElement.value) {
+        editor.value?.dispose();
+        return;
+    };
+    if (!podStore.selectedEntry?.resourceUrl) return;
+
+    const resourceFile = await getFile(
+        podStore.selectedEntry?.resourceUrl,
+        { fetch: getDefaultSession().fetch }
+    );
+
+    editor.value = monaco.editor.create(editorElement.value, {
+        value: await resourceFile.text(),
+        readOnly: true,
+    });
+
+})
+
 </script>
 
 <style scoped>
@@ -83,11 +117,24 @@ a {
     justify-content: space-between;
     align-items: center;
 }
+
+.header-actions {
+    display: flex;
+    justify-content: space-between;
+    gap: .5rem;
+    align-items: center;
+}
+
+.editor {
+    height: 100%;
+}
 </style>
-<style src="@vueform/multiselect/themes/default.css"></style>
 <style>
 .permission-drawer {
     width: 90vw !important;
 }
+
+.content-drawer {
+    width: 70vw !important;
+}
 </style>
-<style src="@vueform/multiselect/themes/default.css"></style>
