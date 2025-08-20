f8381baa8db964fcfcf22dd73d35965d5b58724c
Bram Comyn <bram.comyn@ugent.be>
2025-08-20 16:10:33 +0200

    feat: provided hot swapping of controller instances in controller store

--- loama/src/stores/useControllerStore.ts
+++ loama/src/stores/useControllerStore.ts
@@ -1,16 +1,28 @@
-import { createBasicController } from "loama-controller";
+import { createBasicController, type IController, type PublicSubject, type WebIdSubject } from "loama-controller";
 import { defineStore } from "pinia";
 
 // This store holds all references to the controller the application needs.
 // It holds a mapping of controller types to their respective controller.
-// ATM it only holds one: specifically for ODRL
+// ATM it only holds one: specifically for ODRL.
+
+// perhaps it would be wiser to make some enumeration of all supported AS types
 export const useControllerStore = defineStore("controller", {
     state: () => ({
-        controller: createBasicController(),
+        controller: createBasicController() as IController<{ webId: WebIdSubject; public: PublicSubject }>,
+        controllers: new Map<string, IController<{ webId: WebIdSubject; public: PublicSubject }>>([
+            ["odrl", createBasicController()]
+        ])
     }),
     getters: {
         currentController(state) {
             return state.controller;
+        }
+    },
+    actions: {
+        // switches the current controller out with the requested AS type controller
+        switchTo(type: string) {
+            if (this.controllers.has(type)) this.controller = this.controllers.get(type)!;
+            else throw new Error("unrecognized AS type provided");
         }
     }
 });
