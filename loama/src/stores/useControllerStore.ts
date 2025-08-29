91f49ed339394cd3839437cbc21c20fbfe8c3a90
Bram Comyn <bram.comyn@ugent.be>
2025-08-29 09:05:59 +0200

    feat: implemented controller store

--- loama/src/stores/useControllerStore.ts
+++ loama/src/stores/useControllerStore.ts
@@ -1,28 +1,37 @@
 import { createBasicController, type IController, type PublicSubject, type WebIdSubject } from "loama-controller";
 import { defineStore } from "pinia";
 
-// This store holds all references to the controller the application needs.
-// It holds a mapping of controller types to their respective controller.
-// ATM it only holds one: specifically for ODRL.
+type ControllerType = 'drive' | 'odrl' | 'solid';
+type Controller = IController<{ webId: WebIdSubject; public: PublicSubject }>
 
-// perhaps it would be wiser to make some enumeration of all supported AS types
+interface State {
+    controllers: Map<ControllerType, Controller>;
+    currentControllerType: ControllerType;
+    authorizationServerURL: string;
+}
+
 export const useControllerStore = defineStore("controller", {
-    state: () => ({
-        controller: createBasicController() as IController<{ webId: WebIdSubject; public: PublicSubject }>,
-        controllers: new Map<string, IController<{ webId: WebIdSubject; public: PublicSubject }>>([
-            ["odrl", createBasicController()]
-        ])
+    state: (): State => ({
+        controllers: new Map([ 
+            [ 'odrl', createBasicController() ],
+        ]),
+        currentControllerType: 'odrl',
+        authorizationServerURL: 'http://localhost:4000/'
     }),
     getters: {
-        currentController(state) {
-            return state.controller;
-        }
-    },
-    actions: {
-        // switches the current controller out with the requested AS type controller
-        switchTo(type: string) {
-            if (this.controllers.has(type)) this.controller = this.controllers.get(type)!;
-            else throw new Error("unrecognized AS type provided");
+        types(state) {
+            return state.controllers.keys();
+        },
+        current(state): Controller {
+            if (state.controllers.has(state.currentControllerType)) 
+                return state.controllers.get(state.currentControllerType)!;
+            else throw new Error("No controller selected");
+        },
+        type(state) {
+            return state.currentControllerType;
+        },
+        authorizationServer(state) {
+            return state.authorizationServerURL;
         }
     }
 });
