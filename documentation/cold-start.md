8844c17d9277a16c898f1cf2f2a94f50d7250bde
Bram Comyn <bram.comyn@ugent.be>
2025-09-15 10:09:41 +0200

    feat: updated cold start script + its documentation

--- documentation/cold-start.md
+++ documentation/cold-start.md
@@ -1,35 +1,30 @@
 # The Cold Start Problem
 
 LOAMA allows users to manage their own user-managed policies for content access.
-It does this by communicating with the backend AS and adding rules or stripping them away from existing policies.
-This approach introduces one problem: LOAMA cannot create new policies on new resources from its user interface.
-We introduced a (temporary) script to help users quickly setup policies on new resources. This way, you don't have to manually use curl commands to setup your initial policies.
+It does this by communicating with the backend Authorization Server (AS) and adding rules or stripping them away from existing policies.
 
-The image below shows LOAMA's empty homepage. There is no option to create a new policy for a resource, as such we need [this script](../scripts/cold-start.ts) to populate the AS for new users.
+This approach introduces one problem: LOAMA cannot create new policies on resources from its user interface.
+We introduced a (temporary) script to help quickly set up policies on new resources.
+This way, you don't have to manually use curl commands to set up your initial policies.
 
-![LOAMA's empty home screen](loama_empty.png)
+The image below shows LOAMA's empty homepage.
+There is no option to create a new policy for a resource, as such we need this script to populate the AS for new users.
 
-The script is pretty straight forward in use.
-See the example below:
+![LOAMA's empty homepage](./loama-homepage-empty.png)
+
+The script is straightforward in use, but allows for some customization.
+The example below illustrates its use:
 
 ```shell-session
-$ npm run cold
-
-> cold
-> ts-node scripts/cold-start
-
-Please provide your own WebID for authorization: https://solidweb.me/harrypodder/profile/card#me
-What do you want to name your policy? policy
-What do you want your rule to be named? permission
-What action do you want to associate? Choose one below: 
-	- read    [1]
-	- write   [2]
-	- append  [3]
-	- create  [4]
-	- control [5]
-1
-Please enter the URL to the resource: http://localhost:3000/harrypodder/resource.txt
-Please provide the WebID for the assignee: https://example.pod.knows.idlab.ugent.be/profile/card#me
+> yarn cold
+yarn run v1.22.22
+$ tsx --env-file=.env scripts/cold-start
+To use default ENV vars, leave empty.
+Please provide your own WebID for authorization: 
+What do you want to name your policy? 
+Please enter the URL to the resource: 
+Please provide the WebID for the assignee: 
+Enter actions to associate (comma separated). Options: read, write, append, create, control: read
 
 POST request with body
 
@@ -37,17 +32,39 @@ POST request with body
         @prefix odrl: <http://www.w3.org/ns/odrl/2/> .
         @prefix dct: <http://purl.org/dc/terms/>.
 
-        ex:policy a odrl:Agreement .
-        ex:policy odrl:permission ex:permission .
-        ex:permission a odrl:Permission .
-        ex:permission odrl:action odrl:read .
-        ex:permission odrl:target <http://localhost:3000/harrypodder/resource.txt> .
-        ex:permission odrl:assignee <https://example.pod.knows.idlab.ugent.be/profile/card#me> .
-        ex:permission odrl:assigner <https://solidweb.me/harrypodder/profile/card#me> .
+        ex:07e08a26-9655-4b63-aa6b-5ac2ec0ddaa5 a odrl:Agreement ;
+                     odrl:permission ex:adecea94-275d-4e48-8f8d-2a735903502f ;
+                     odrl:uid ex:07e08a26-9655-4b63-aa6b-5ac2ec0ddaa5 .
+        
+        ex:adecea94-275d-4e48-8f8d-2a735903502f a odrl:Permission ;
+                   odrl:action odrl:read ;
+                   odrl:target <http://localhost:3000/resources/95b952cb-c680-47b7-83c3-c8356926e470> ;
+                   odrl:assignee <https://example.pod.knows.idlab.ugent.be/profile/card#me> ;
+                   odrl:assigner <https://solidweb.me/harrypodder/profile/card#me> .
     
-Policy added succesfully
+Policy added successfully
+Done in 7.51s.
 ```
 
-This will create the following result in LOAMA:
+LOAMA's frontend will now show the policy for the resource:
 
-![LOAMA's homescreen showing the added policy](loama_policy.png)
+![LOAMA's homepage showing the policy](./loama-homepage-filled.png)
+
+## Default variables in .env files
+
+It is possible to define some default variables.
+The script will read these from the .env file and use these values if the answers are left empty.
+If no .env file is provided, the script will use fallback default values.
+
+These default values can be summarized in this example .env file:
+
+```env
+ASSIGNEE_IRI="https://example.pod.knows.idlab.ugent.be/profile/card#me"
+ASSIGNER_IRI="https://example.pod.knows.idlab.ugent.be/profile/card#me"
+RESOURCE_SERVER="http://localhost:3000/"
+AUTHORIZATION_SERVER="http://localhost:4000/"
+```
+
+Notice the trailing slashes for the resource and authorization servers.
+The script assumes that the policy endpoint is running under `/uma/policies`.
+Resources are always created under the `/resources` path.
--- scripts/cold-start.ts
+++ scripts/cold-start.ts
@@ -1,18 +1,18 @@
 /**
  * This script serves to solve the cold start problem for LOAMA.
- * It is equivalent to a series of configurable cURL requests being sent towards the UMA AMA server
- * and thus entirely based on the examples provided in [the policy management endpoint documentation](https://github.com/SolidLabResearch/user-managed-access/blob/feat/policy-endpoint/documentation/policy-management.md)
+ * It creates a new policy with one or more actions attached to the same target and assignee.
+ * Based on the examples provided in the UMA AMA policy management documentation.
  */
 
-import { Permission } from "../controller/src/types";
 import { v4 as uuidv4 } from 'uuid';
 import * as readline from 'node:readline';
 
 const defaultAssignee = process.env.ASSIGNEE_IRI || "https://example.pod.knows.idlab.ugent.be/profile/card#me";
-const defaultAssigner = process.env.ASSIGNER_IRI || 'https://example.pod.knows.idlab.ugent.be/profile/card#me';
+const defaultAssigner = process.env.ASSIGNER_IRI || "https://example.pod.knows.idlab.ugent.be/profile/card#me";
 
 const getRandomName = () => uuidv4();
-const getRandomResourceName = () => `${process.env.RESOURCE_SERVER || 'http://localhost:3000'}/resources/${uuidv4()}`
+const getRandomResourceName = () =>
+    `${process.env.RESOURCE_SERVER || 'http://localhost:3000/'}resources/${uuidv4()}`;
 
 const rl = readline.createInterface({
     input: process.stdin,
@@ -20,87 +20,68 @@ const rl = readline.createInterface({
 });
 
 const askBasicQuestion = async (question: string) => {
-    return new Promise((resolve) => {
-        rl.question(question, (answer: string) => resolve(answer));
+    return new Promise<string>((resolve) => {
+        rl.question(question, (answer: string) => resolve(answer.trim()));
     });
 };
 
 const askWebId = async () => askBasicQuestion('Please provide your own WebID for authorization: ');
 const askPolicyName = async () => askBasicQuestion('What do you want to name your policy? ');
-const askRuleName = async () => askBasicQuestion('What do you want your rule to be named? ');
 const askTargetURL = async () => askBasicQuestion('Please enter the URL to the resource: ');
 const askAssigneeId = async () => askBasicQuestion(`Please provide the WebID for the assignee: `);
 
-const selectAction = () => {
-    return new Promise((resolve, reject) => {
-        rl.question(
-            'What action do you want to associate? Choose one below: \n\t- read    [1]\n\t- write   [2]\n\t- append  [3]\n\t- create  [4]\n\t- control [5]: ',
-            (answer) => {
-                let choice = parseInt(answer, 10);
-                if (choice < 1 || choice > 5) reject("No valid option provided");
-
-                let permission: Permission;
-                
-                switch (choice) {
-                    case 1 : { 
-                        permission = Permission.Read;
-                        break;
-                    } case 2 : {
-                        permission = Permission.Write;
-                        break;
-                    } case 3 : {
-                        permission = Permission.Append;
-                        break;
-                    } case 4 : {
-                        permission = Permission.Create;
-                        break;
-                    } case 5 : {
-                        permission = Permission.Control;
-                        break;
-                    } default : {
-                        permission = Permission.Read;
-                        break;
-                    }
-                }
-
-                resolve(permission.toLowerCase());
-            }
-        );
-    });
-}
+const askActions = async () => {
+    const answer = await askBasicQuestion(
+        'Enter actions to associate (comma separated). Options: read, write, append, create, control: '
+    );
+    return answer
+        .split(",")
+        .map(a => a.trim().toLowerCase())
+        .filter(a => a); // remove empty entries
+};
 
 const main = async () => {
-    console.log("To use default ENV vars, leave empty.")
-    
-    // ask user for webid, policy and rule names, target, associated action and assignee
+    console.log("To use default ENV vars, leave empty.");
+
     const webid = await askWebId() || defaultAssigner;
     const policy = await askPolicyName() || getRandomName();
-    const rule = await askRuleName() || getRandomName();
-    const action = await selectAction();
     const target = await askTargetURL() || getRandomResourceName();
     const assignee = await askAssigneeId() || defaultAssignee;
 
+    const actions = await askActions();
+
     rl.close();
 
-    // make POST request to the API
+    let rules: string[] = [];
+    let ruleRefs: string[] = [];
+
+    for (const action of actions) {
+        const rule = getRandomName();
+
+        rules.push(`
+        ex:${rule} a odrl:Permission ;
+                   odrl:action odrl:${action} ;
+                   odrl:target <${target}> ;
+                   odrl:assignee <${assignee}> ;
+                   odrl:assigner <${webid}> .`);
+
+        ruleRefs.push(`ex:${rule}`);
+    }
+
     const body = `
         @prefix ex: <http://example.org/>.
         @prefix odrl: <http://www.w3.org/ns/odrl/2/> .
         @prefix dct: <http://purl.org/dc/terms/>.
 
         ex:${policy} a odrl:Agreement ;
-                     odrl:uid ex:${policy} ;
-                     odrl:permission ex:${rule} .
-        ex:${rule} a odrl:Permission ;
-                   odrl:action odrl:${action} ;
-                   odrl:target <${target}> ;
-                   odrl:assignee <${assignee}> ;
-                   odrl:assigner <${webid}> .
-    `
+                     ${ruleRefs.map(r => `odrl:permission ${r} ;`).join("\n                     ")}
+                     odrl:uid ex:${policy} .
+        ${rules.join("\n")}
+    `;
 
     console.log(`\nPOST request with body\n${body}`);
 
-    const response = await fetch(`${process.env.AUTHORIZATION_SERVER || 'http://localhost:4000'}/uma/policies`, {
+    const response = await fetch(`${process.env.AUTHORIZATION_SERVER || 'http://localhost:4000/'}uma/policies`, {
         method: 'POST',
         headers: {
             'Authorization': `${webid}`,
@@ -110,10 +91,10 @@ const main = async () => {
     });
 
     if (response.ok) {
-        console.log('Policy added succesfully');
+        console.log('Policy added successfully');
     } else {
         console.log('Something went wrong! Try again.', response.status);
     }
-}
+};
 
-main()
+main();
